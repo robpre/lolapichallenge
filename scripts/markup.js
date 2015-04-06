66f3f76115b6a8cd6d53a98b3a339672e66561f0
Robert Preus-MacLaren <rpprroger@googlemail.com>
2015-04-06 20:38:10 +0100

    Fixed stupid ass copy thing

--- scripts/markup.js
+++ scripts/markup.js
@@ -1,21 +1,47 @@
 var Q = require('q');
-var cp = require('glob-copy');
+var glob = require('glob');
+var fs = require('fs');
 
-module.exports = function(root) {
-	var path = root.replace(/[^\/]$/, function(str, match) {
-		return match + '/';
-	});
+function copyFile(from, to) {
 	var copy = Q.defer();
-console.log(path);
-	cp(path + '/client/**/*.html', path + '/public', function(err, files) {
-		if( err ) {
-			console.log('aahhh fuck');
-			copy.reject();
-		} else {
-			console.log('copied', files);
-			copy.resolve();
-		}
-	});
-
+	// console.log('copying from', from);
+	// console.log('to', to);
+	fs.createReadStream(from)
+		.pipe(fs.createWriteStream(to))
+		.on('done', copy.resolve)
+		.on('error', copy.reject);
 	return copy.promise;
 }
+
+module.exports = function(root) {
+	var path = process.cwd() + '/';
+	var htmlGlob = 'client/**/*.html'; 
+	var cssGlob = 'client/**/*.css';
+
+	var html = Q.defer();
+	var css = Q.defer();
+
+	function handler(defer) {
+		return function(err, files) {
+			if( err ) {
+				console.log('failed! ', err);
+				defer.reject();
+			} else {
+				var fileMap = files.map(function(filePath) {
+					var newPath = filePath.replace(path + 'client', path + 'public');
+					return copyFile(filePath, newPath);
+				});
+
+				Q.all(fileMap).then(defer.resolve);
+			}
+		};
+	}
+
+	glob(path + htmlGlob, handler(html));
+
+	glob(path + cssGlob, handler(css));
+
+	return Q.all([html.promise, css.promise]);
+}
+
+module.exports();
