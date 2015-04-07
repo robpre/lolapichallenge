321087b849a9880776da8880a4a82214a8a49199
Stansbridge <github@anthonystansbridge.co.uk>
2015-04-07 17:52:24 +0100

    Added a new exploration endpoint for the Riot match endpoint

--- server/index.js
+++ server/index.js
@@ -6,21 +6,35 @@ var qs = require('querystring');
 module.exports = function(apiKey) {
 	var api = express();
 	
-	/*api.get('/*', function(res, req/*) {
-		request(sameUr )
-	});*/
-	api.get('/urf', function(req, res) {
-		debug('doing request with ' + apiKey);
-		debug('https://euw.api.pvp.net/api/lol/euw/v4.1/game/ids?' + qs.stringify({
+	api.get('/urf/retrieve-games', function(req, res) {
+		var requestUrl = 'https://euw.api.pvp.net/api/lol/euw/v4.1/game/ids?' + qs.stringify({
 				beginDate: (new Date('2015-04-06T12:00')).getTime()/1000,
 				api_key: apiKey
-			}));
+		});
+
+		debug('Doing request with ' + apiKey);
+		debug(requestUrl);
+
 		request({
-			url: 'https://euw.api.pvp.net/api/lol/euw/v4.1/game/ids?' + qs.stringify({
-				beginDate: (new Date('2015-04-06T12:00')).getTime()/1000,
-				api_key: apiKey
-			})
+			url: requestUrl
 		}).pipe(res);
+	});
+
+	api.get('/urf/find-game/:matchId', function(req, res) {
+		var matchId = req.params.matchId;
+		var requestUrl = 'https://euw.api.pvp.net/api/lol/euw/v2.2/match/' + matchId + '?' + qs.stringify({
+				includeTimeline: true,	
+				api_key: apiKey
+		});
+
+		debug('Doing request with ' + apiKey);
+		debug('Retrieving match ' + matchId);
+		debug(requestUrl);
+
+		request({
+			url: requestUrl
+		})
+		.pipe(res);
 	});
 
 	return api;
