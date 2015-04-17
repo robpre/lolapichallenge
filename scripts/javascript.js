d1cf073c810fb391b7d29434110bc3a7f3a2526e
Robert Preus-MacLaren <rpprroger@googlemail.com>
2015-04-17 20:33:04 +0100

    SOOPID Memory leak

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -2,6 +2,9 @@ var browserify = require('browserify');
 var Q = require('q');
 var fs = require('fs');
 var debug = require('debug')('urf:scripts:javascript');
+var excorsist = require('exorcist');
+
+var dev = process.env.NODE_ENV === 'development';
 
 module.exports = function(bundle) {
 	// relative from project root
@@ -11,14 +14,19 @@ module.exports = function(bundle) {
 	debug('Compiling ', scripts.join(''), ' to ', bundle);
 	return Q.Promise(function(resolve, reject) {
 		var b = browserify({
-			debug: process.env.NODE_ENV === 'development'
+			debug: dev
 		});
 
 		scripts.forEach(function( scriptPath ) {
 			b.add(scriptPath);
 		});
 
-		b.bundle().pipe(fs.createWriteStream(bundle + 'public/js/bundle.js'))
+		var dataStream = b.bundle();
+
+		if(dev) {
+			dataStream = dataStream.pipe(excorsist(bundle + 'public/js/bundle.js.map'));
+		}
+		dataStream.pipe(fs.createWriteStream(bundle + 'public/js/bundle.js'))
 			.on('finish', resolve)
 			.on('error', reject);
 	});
