dc6693ff8e2676b06910bdc82800ce31910dc829
Robert Preus-MacLaren <rpprroger@googlemail.com>
2015-04-19 18:16:11 +0100

    Fix some server bugs

--- server/configure_socket.js
+++ server/configure_socket.js
@@ -52,8 +52,11 @@ ConfigureSocket.prototype.handle = function(socket, session) {
 			return error(true, 'not in a game', 'recoverable');
 		}
 		var activeGame = _.findWhere(handler.games, {id: session.activeGameID});
-		if(!error(activeGame, 'game not found', 'recoverable')) {
+		if(!error(!activeGame, 'game not found', 'recoverable')) {
 			callback(activeGame);
+		} else {
+			session.activeGameID = null;
+			session.save();
 		}
 	}
 
@@ -78,7 +81,6 @@ ConfigureSocket.prototype.handle = function(socket, session) {
 				// if there's a deck size miss-match treat the request as a failure
 				if(!error(deck.length !== cards.length || cards.length !== 9, 'card number miss-match', 'recoverable')) {
 					// otherwise continue
-					debug(cards);
 					var game = handler.findGame();
 					var playerObj = {
 						user: userObj.username,
@@ -95,7 +97,6 @@ ConfigureSocket.prototype.handle = function(socket, session) {
 					}
 
 					session.activeGameID = game.id;
-					debug(game);
 					session.save(function(err) {
 						if(!error(err, 'error saving game to session', 'recoverable')) {
 							debug('game is ready: ', game.isReady());
@@ -115,7 +116,6 @@ ConfigureSocket.prototype.handle = function(socket, session) {
 	});
 
 	socket.on('action', function(target, actionName, card, where, stat) {
-		debug(target, actionName, card, where, stat);
 		findUser(function(userObj) {
 			getGame(function(activeGame) {
 				activeGame.action(userObj.username, target, actionName, card, where, stat, function(err) {
@@ -145,9 +145,10 @@ ConfigureSocket.prototype.handle = function(socket, session) {
 			for (var i = 0; i < handler.games[j].clients.length; i++) {
 				if(handler.games[j].clients[i].socket === socket) {
 					handler.closeGame(handler.games[j]);
+					break;
 				}
 			}
-		}		
+		}
 	});
 
 	socket.on('end turn', function() {
